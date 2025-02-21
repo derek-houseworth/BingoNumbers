903ea30f9effdd3f05a2f1049cfdeb5911e62e8c
derek.houseworth <derek@houseworthconsulting.com>
2025-02-21 08:30:50 -0700

    update NuGet packages, fix build warnings from InvertBoolConverter

---
+++ BingoNumbers/Views/InvertBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Globalization;
+
+namespace BingoNumbers.Views
+{
+	public  class InvertBoolConverter:IValueConverter
+	{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			return value != null && !(bool)value;
+		}
+
+		public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			return value != null && !(bool)value;
+		}
+	}
+}
