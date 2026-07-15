9b7594c8707ec546c9f684a9770f07556ee9beb6
derek-houseworth <derek@houseworthconsulting.com>
2026-07-14 20:19:08 -0600

    uypdate NuGet package versions

---
+++ BingoNumbers/Converters/InvertBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace BingoNumbers.Converters;
+
+public  class InvertBoolConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+
+		return value != null && !(bool)value;
+	
+	} //Convert
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+
+		return value != null && !(bool)value;
+
+    } //ConvertBack
+
+} //InvertBoolConverter
